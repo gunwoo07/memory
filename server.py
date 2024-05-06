b30dc724964615d2f65d5f2419e03d89ea3e559a
gunwoo07 <gunwoo07@github.com>
2024-05-06 13:18:34 +0900

    final

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import random
 
 app = Flask(__name__)
 data_dir = os.getcwd() + '/data'
+password = "play"
 
 @app.route('/')
 def home():
@@ -14,27 +15,31 @@ def home():
 @app.route('/upload', methods=['POST'])
 def upload():
     image = request.files['file']
+    tried_pw = request.form['pw']
     file_name = image.filename
-    if '.' in file_name and file_name.split('.')[-1] in ['jpg', 'jpeg', 'png', 'bmp']:
+    if tried_pw == password and '.' in file_name and file_name.split('.')[-1] in ['jpg', 'jpeg', 'png', 'bmp']:
         ext = file_name.split('.')[-1]
         new_file_name = f'{time.strftime("%Y%m%d-%H%M%S-") + str(random.randint(0, 99))}.{ext}'
         image.save(data_dir+'/'+new_file_name)
     return redirect('/')
 
-@app.route('/image/<file_name>')
-def image(file_name):
-    image_list = os.listdir(data_dir)
-    secured_file_name = secure_filename(file_name)
-    
-    for image in image_list:
-        if image.split('.')[0] == secured_file_name:
-            return send_from_directory(data_dir, image)
+@app.route('/image/<file_name>/<tried_pw>')
+def image(file_name, tried_pw):
+    if tried_pw == password:
+        image_list = os.listdir(data_dir)
+        secured_file_name = secure_filename(file_name)
+        
+        for image in image_list:
+            if image.split('.')[0] == secured_file_name:
+                return send_from_directory(data_dir, image)
 
     return redirect('/')
 
-@app.route('/imagelist')
-def image_list():
-    return jsonify({'image_list':[a.split('.')[0] for a in os.listdir(data_dir)]})
+@app.route('/imagelist/<tried_pw>')
+def image_list(tried_pw):
+    if tried_pw == password:
+        return jsonify({'image_list':[a.split('.')[0] for a in os.listdir(data_dir)]})
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
